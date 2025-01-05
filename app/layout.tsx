199213d5c11b5431c67dbaf3a89ff942e9c48e55
Harsh Vardhan <110846831+mus49@users.noreply.github.com>
2025-01-05 23:13:13 +0530

    Update layout.tsx vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from '@/components/header'
@@ -37,6 +38,7 @@ export default function RootLayout({
             </div>
           </footer>
         </ThemeProvider>
+        <Analytics/>
       </body>
     </html>
   )
